79998644891a3ef4888e375ef99cb7c8d5b84570
LZAMOR15 <luis.zamora@dxc.com>
2025-10-08 13:53:00 -0600

    Here goes nothing!

---
+++ helloTimer/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Threading.Tasks;
+
+Console.WriteLine("Hola mundo");
+await Task.Delay(TimeSpan.FromSeconds(3));
+Console.WriteLine("Terminó el timer");
---
+++ testTheThing/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace HolaTimer.Tests;
+
+[TestClass]
+public class TimerTests
+{
+    [TestMethod]
+    public async Task EsperaDiezSegundosYLoguea()
+    {
+        Console.WriteLine("Iniciando prueba: esperando 3 segundos...");
+        await Task.Delay(TimeSpan.FromSeconds(3));
+        Console.WriteLine("Terminó el timer (prueba).");
+        Assert.IsTrue(true); // dummy assert para que cuente como pasada
+    }
+}
